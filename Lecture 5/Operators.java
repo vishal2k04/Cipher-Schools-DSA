2500e2b12d1809a7243bb4c5c124876d9681d047
VISHAL KUMAR SINGH <122630855+vishal2k04@users.noreply.github.com>
2024-06-13 20:31:31 +0530

    Add files via upload

---
+++ Lecture 5/Operators.java
@@ -0,0 +1,22 @@
+public class Operators{
+    public static void main(String args[]){
+        
+        float f =(float)5.5;
+        System.out.println(f);
+        float f2 = 5.5f;
+        System.out.println(f2);
+        char ch = 'a';
+        System.out.println(ch);
+        //int z=x+y;
+        // + -> operator 
+        // x,y -> operands
+        int m=5;
+        int n=m++;
+        System.out.println("M is: "+m+",N is: "+n);
+
+        int p=5;
+        int q=++p;
+        System.out.println("P is: "+p+", Q is: "+q);
+
+    }
+}
